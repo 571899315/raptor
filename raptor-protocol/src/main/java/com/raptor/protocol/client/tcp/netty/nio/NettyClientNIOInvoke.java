06ca17ff7238104a65f3ce49d59df46bbf30b7a4
chenkang <chenkang@cathay-inc.com.cn>
2018-09-06 15:14:18 +0800

    去除lombok

--- raptor-protocol/src/main/java/com/raptor/protocol/client/tcp/netty/nio/NettyClientNIOInvoke.java
+++ raptor-protocol/src/main/java/com/raptor/protocol/client/tcp/netty/nio/NettyClientNIOInvoke.java
@@ -1,15 +1,24 @@
 package com.raptor.protocol.client.tcp.netty.nio;
 
 
+import com.raptor.common.config.ClientConfig;
+import com.raptor.common.model.RPCRequest;
+import com.raptor.common.model.RPCResponse;
+import com.raptor.common.model.ServiceAddress;
+import com.raptor.loadbalancer.loadbalance.AbstractLoadBalance;
+
+import java.util.List;
 
 /***
  * netty nio 实现
  */
-public class NettyClientNIOInvoke   {
+public class NettyClientNIOInvoke extends AbstractLoadBalance {
 
+    @Override
+    public RPCResponse doInvoke(RPCRequest request, ClientConfig config, List<ServiceAddress> addresses) throws Exception {
 
-   
-    protected Object doInvoke() {
+        System.out.print("NettyClientNIOInvoke invoke ");
+
         return null;
     }
 }

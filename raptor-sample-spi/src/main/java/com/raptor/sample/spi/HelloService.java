2d4a75faf0f407c1ef0f25b1c69458ea9cd7910b
chenkang <chenkang@cathay-inc.com.cn>
2018-09-10 13:07:51 +0800

    项目优化
    项目优化

--- raptor-sample-spi/src/main/java/com/raptor/sample/spi/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.raptor.sample.spi;
-
-import com.raptor.common.annotation.RaptorService;
-
-@RaptorService(HelloService.class)
-public interface HelloService {
-
-	String hello(String name);
-}

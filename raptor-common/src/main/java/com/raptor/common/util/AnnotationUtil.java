7bdd45a59485201016d4cc3a31901c1c0a136fcf
chenkang <chenkang@qq.com>
2018-09-11 11:32:55 +0800

    服务端添加处理流程

---
+++ raptor-common/src/main/java/com/raptor/common/util/AnnotationUtil.java
@@ -0,0 +1,27 @@
+package com.raptor.common.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.raptor.common.annotation.RaptorServer;
+
+public class AnnotationUtil {
+
+	public static List<Class<?>> listByAnnotation(String[] packageNames) {
+		List<Class<?>> clsList = ClassUtil.getClasses(packageNames);
+		List<Class<?>> result = new ArrayList<>();
+		if (clsList != null && clsList.size() > 0) {
+			for (Class<?> cls : clsList) {
+				Class<?>[] declaredClasses = cls.getDeclaredClasses();
+				for (Class<?> clazz : declaredClasses) {
+					boolean annotation = clazz.isAnnotationPresent(RaptorServer.class);
+					if (annotation) {
+						result.add(clazz);
+					}
+				}
+
+			}
+		}
+		return result;
+	}
+}
--- rpcx-example/rpcx-provider/src/main/java/com/rpc/rpcx/UserServiceImpl.java
+++ rpcx-example/rpcx-provider/src/main/java/com/rpc/rpcx/UserServiceImpl.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.raptor.common.annotation.RaptorServer;
+
 @Service
 @RaptorServer(UserServiceImpl.class)
 public class UserServiceImpl implements UserService {

64b7b23d5ef676b9ba264df3ebb6eb7e73aa21f2
chenkang <chenkang@cathay-inc.com.cn>
2018-09-12 16:39:59 +0800

    init

---
+++ rpcx-example/rpcx-interface/src/main/java/com/rpc/rpcx/Address.java
@@ -0,0 +1,23 @@
+package com.rpc.rpcx;
+
+public class Address {
+
+    private int code;
+    private String address;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
